9a1f6062d97a0fef99a87d7bacce674c99498d42
jp-96 <jp-96@outlook.jp>
2022-06-16 10:36:55 +0000

    Advanced Star Component

--- src/app/StarRating.tsx
+++ src/app/StarRating.tsx
@@ -1,12 +1,21 @@
-import React from "react";
-import { FaStar } from "react-icons/fa";
+import React, { useState } from "react";
+import Star from "./Star";
+import { createArray } from "./lib";
 
-export default function StarRating() {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />
-  ];
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <div style={{ padding: 5, ...style }} {...props}>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </div>
+  );
 }
